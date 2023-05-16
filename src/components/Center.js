19f8bb80fe4ad9b9ab3998e5b8ed3e08276bbfdf
ithmv <ithmv@vk.com>
2023-05-16 22:56:06 +0700

    first commit

--- src/components/Center.js
+++ src/components/Center.js
@@ -1,31 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Tile from "./Tile";
 import { getComments } from "../data/api";
 
 function Center() {
   const [names, setNames] = useState([]);
+  const centerRef = useRef(null);
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if (centerRef.current) {
+      const tileWidth = 180; // ширина плитки с учетом margin
+      const tileHeight = 130; // высота плитки с учетом margin
+      const centerElement = centerRef.current;
+      const centerWidth = centerElement.offsetWidth - 60; // ширина поля Center с учетом отступов
+      const centerHeight = centerElement.offsetHeight - 60; // высота поля Center с учетом отступов
+      const maxTilesHorizontally = Math.floor(centerWidth / tileWidth);
+      const maxTilesVertically = Math.floor(centerHeight / tileHeight);
+      const maxTiles = maxTilesHorizontally * maxTilesVertically;
+      const MAX_TILES = maxTiles;
+      setMaxTiles(MAX_TILES);
+    }
+  }, [centerRef]);
+
   async function fetchData() {
     try {
       const comments = await getComments();
-      const commentNames = comments.map(comment => comment.name);
+      const commentNames = comments.map((comment) => comment.name);
       setNames(commentNames);
     } catch (error) {
-      console.error('Ошибка при получении данных:', error);
+      console.error("Ошибка при получении данных:", error);
     }
   }
 
-  const MAX_TILES = 220; // Максимальное количество отображаемых компонентов Tile
-
-  const visibleNames = names.slice(0, MAX_TILES); // Получаем только первые MAX_TILES имен
+  const [maxTiles, setMaxTiles] = useState(0);
 
   return (
-    <div className="center">
-      {visibleNames.map((name, index) => (
+    <div className="center" ref={centerRef}>
+      {names.slice(0, maxTiles).map((name, index) => (
         <Tile key={index} name={name} />
       ))}
     </div>
